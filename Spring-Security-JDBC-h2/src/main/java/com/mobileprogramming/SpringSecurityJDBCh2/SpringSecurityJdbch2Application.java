68ba6beaec1f8ea52ded2502a72e7747177be9ae
Lenovo <Lenovo@DESKTOP-N0I1SF6>
2019-12-29 16:24:08 +0530

    spring jdbc authentication using h2 database

---
+++ Spring-Security-JDBC-h2/src/main/java/com/mobileprogramming/SpringSecurityJDBCh2/SpringSecurityJdbch2Application.java
@@ -0,0 +1,13 @@
+package com.mobileprogramming.SpringSecurityJDBCh2;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringSecurityJdbch2Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringSecurityJdbch2Application.class, args);
+	}
+
+}
---
+++ Spring-Security-JDBC-h2/src/test/java/com/mobileprogramming/SpringSecurityJDBC2/SpringSecurityJdbc2ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.mobileprogramming.SpringSecurityJDBC2;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringSecurityJdbc2ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
