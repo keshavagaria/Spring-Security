edda263a7c16b63c9eb2f1fd5889aa4c2324d694
Lenovo <Lenovo@DESKTOP-N0I1SF6>
2019-12-31 20:01:45 +0530

    spring mvc with hibernate using spring security demo

---
+++ SpringMVCHibernateWithSpringSecurityExample/src/main/java/com/mobileprogramming/dao/UserProfileDao.java
@@ -0,0 +1,15 @@
+package com.mobileprogramming.dao;
+
+import java.util.List;
+
+import com.mobileprogramming.model.UserProfile;
+
+
+public interface UserProfileDao {
+
+	List<UserProfile> findAll();
+	
+	UserProfile findByType(String type);
+	
+	UserProfile findById(int id);
+}
---
+++ SpringMVCHibernateWithSpringSecurityExample/src/main/java/com/mobileprogramming/model/UserProfileType.java
@@ -0,0 +1,20 @@
+package com.mobileprogramming.model;
+
+import java.io.Serializable;
+
+public enum UserProfileType implements Serializable{
+	USER("USER"),
+	DBA("DBA"),
+	ADMIN("ADMIN");
+	
+	String userProfileType;
+	
+	private UserProfileType(String userProfileType){
+		this.userProfileType = userProfileType;
+	}
+	
+	public String getUserProfileType(){
+		return userProfileType;
+	}
+	
+}
